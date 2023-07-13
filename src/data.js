c14693776e0179d0f6f60a4e32f05039827553f0
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2023-07-13 12:56:51 -0400

    react router v6.4 demo

---
+++ src/data.js
@@ -0,0 +1,21 @@
+import tacomaSEImage from "./images/tacoma-se-2023.jpg";
+import tacomaSRImage from "./images/tacoma-sr-2023.webp";
+import trdSportImage from "./images/TRD-sport-2023.png";
+import trdOffRoadImage from "./images/TRD-off-road-2023.jpg";
+import trdProImage from "./images/TRD-Pro-2023.jpg";
+
+const data = [
+  { id: 1, make: "toyota", model: "tacoma", trim: "se", img: tacomaSEImage },
+  { id: 2, make: "toyota", model: "tacoma", trim: "sr", img: tacomaSRImage },
+  { id: 3, make: "toyota", model: "tacoma", trim: "sport", img: trdSportImage },
+  {
+    id: 4,
+    make: "toyota",
+    model: "tacoma",
+    trim: "off-road",
+    img: trdOffRoadImage,
+  },
+  { id: 5, make: "toyota", model: "tacoma", trim: "pro", img: trdProImage },
+];
+
+export default data;
